e41fc4fab13b3f04944c8705999a3be94ba31dd4
Márcio <marciowinicius.mw@gmail.com>
2021-09-27 09:46:10 -0300

    SH PM2.

--- execute_pm.sh
+++ execute_pm.sh
@@ -8,16 +8,8 @@ pm2 stop bot2
 
 sleep 1
 
-pm2 stop bot3
-
-sleep 1
-
 pm2 start bot1 --watch
 
-sleep 15
+sleep 30
 
-pm2 start bot2 --watch
-
-sleep 15
-
-pm2 start bot3 --watch
+pm2 start bot2 --watch=

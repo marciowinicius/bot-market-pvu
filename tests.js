ba1a3b5f1e7093761922088b41af4d333d52143c
Márcio <marciowinicius.mw@gmail.com>
2021-10-06 12:33:21 -0300

    Adicionar mais alertas.

--- tests.js
+++ tests.js
@@ -478,7 +478,7 @@ async function sendDiscordAlert(webhook, informations) {
         .setText(discordMessageOptions.content)
         .setThumbnail(informations.icon_url)
 
-    hook.send(embed).catch(err => console.log(err, informations));
+    hook.send(embed).catch(err => console.log(err, informations, webhook));
 }
 
 function getDefaultObjDiscordMessage(webhook, informations) {
